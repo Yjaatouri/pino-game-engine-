4725287178ffc2d961235be58aaccd7cce4aa981
yahya Jaatouri <yahyajaatouri2000@gmail.com>
2026-06-17 14:53:20 +0100

    Remove dead CMake options and orphan compile definitions

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,8 +2,3 @@ option(PINO_BUILD_EXAMPLES "Build example projects" ON)
 option(PINO_USE_SYSTEM_SDL2 "Use system-installed SDL2 instead of FetchContent" OFF)
 
 option(PINO_ENABLE_PHYSICS "Enable physics/collision support" ON)
-option(PINO_ENABLE_LOGGING "Enable logging subsystem" ON)
-option(PINO_ENABLE_AUDIO "Enable audio support (placeholder)" OFF)
-
-option(PINO_BUILD_ANDROID "Target Android (requires NDK)" OFF)
-option(PINO_BUILD_IOS "Target iOS (requires Xcode)" OFF)
